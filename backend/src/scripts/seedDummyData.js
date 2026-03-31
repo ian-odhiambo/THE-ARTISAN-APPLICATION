c4057fd00445d7da5d24a2deefe01cc4d39407b6
ian <odhiamboian096>
2026-03-31 14:34:28 +0300

    another commit

--- backend/src/scripts/seedDummyData.js
+++
@@ -1,8 +0,0 @@
-// import mongoose from 'mongoose';
-// import dotenv from 'dotenv';
-// import User from '../../models/User.js';
-// import Product from '../../models/Product.js';
-
-// dotenv.config();
-
-// async function seedData() {
