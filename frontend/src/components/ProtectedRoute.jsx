42d5a053d02908018413ec8258186299d6d73318
ian <odhiamboian096@gmail.com>
2026-05-23 23:50:21 +0300

    debugging the protected routes file

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,18 +1,18 @@
-import { Navigate } from 'react-router-dom';
-import '../index.css';
-const ProtectedRoute = ({ children, allowedRoles = [] }) => {
-  const storedUser = localStorage.getItem('user');
-  const user = storedUser ? JSON.parse(storedUser) : null;
+// import { Navigate } from 'react-router-dom';
+// import '../index.css';
+// const ProtectedRoute = ({ children, allowedRoles = [] }) => {
+//   const storedUser = localStorage.getItem('user');
+//   const user = storedUser ? JSON.parse(storedUser) : null;
 
-  // if not logged in hen redirect
-  if (!user) return <Navigate to="/login" replace />;
+//   // if not logged in hen redirect
+//   if (!user) return <Navigate to="/login" replace />;
 
-  //  if role is not allowed also redirect
-  if (allowedRoles.length && !allowedRoles.includes(user.role)) {
-    return <Navigate to="/" replace />;
-  }
+//   //  if role is not allowed also redirect
+//   if (allowedRoles.length && !allowedRoles.includes(user.role)) {
+//     return <Navigate to="/" replace />;
+//   }
 
-  return children;
-};
+//   return children;
+// };
 
-export default ProtectedRoute;
+// export default ProtectedRoute;
